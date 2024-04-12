21ac401cd3e8b6489a1c719412eb2ac48a23906e
So-Simoneuk <165155995+So-Simoneuk@users.noreply.github.com>
2024-04-12 17:24:08 +0100

    CSS fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+function searchCity(city)
+{
+let apiKey ="14cfdda3fo2fca08bfa9c4f19a5dat70"
+let apiUrl ="https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"
+console.log(apiUrl);
+}
+
 function handleSearchSubmit(event)
 {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML =searchInput.value;
+    searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
