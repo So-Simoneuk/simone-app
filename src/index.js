7a831a1779c62a84e907283f3951c339f84b5ddb
So-Simoneuk <165155995+So-Simoneuk@users.noreply.github.com>
2024-04-12 19:38:52 +0100

    Icon fix

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ function refreshWeather(response)
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
     
     cityElement.innerHTML = response.data.city;
@@ -16,6 +17,7 @@ function refreshWeather(response)
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     timeElement.innerHTML = formatDate(date);
+    iconElement.innerHTML =`<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 }
 
 function formatDate(date) {
